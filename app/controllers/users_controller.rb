c7b21c5c51a3cc506e94ee16af39a020b854bb41
eliastooloee <elitooloee@gmail.com>
2020-03-02 09:27:57 -0800

    added user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def show
+        render json: @user
+    end
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.create(name: params[:name])
+    end
+
+    def destroy
+        @user.destroy
+    end
+
+    def update
+        @user.update(user_params)
+        render json: @user
+    end
+
+    private
+
+    def find_user
+		@user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
--- app/controllers/users_pokemons_controller.rb
+++ app/controllers/users_pokemons_controller.rb
@@ -1,2 +1,5 @@
 class UsersPokemonsController < ApplicationController
+
+    
+
 end
