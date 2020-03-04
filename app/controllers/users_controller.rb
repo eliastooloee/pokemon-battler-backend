6375e887c22071d91013a2d08bab4e66cc11e023
christinamcmahon <christina.mcmahon1@gmail.com>
2020-03-03 17:21:36 -0800

    fix associations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @users = User.all
+    render json: @users
+  end
+
   def show
     render json: @user
   end
