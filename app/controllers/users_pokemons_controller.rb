2fbea55224791bcf8ddf8b130c775e0ece14fe81
eliastooloee <elitooloee@gmail.com>
2020-03-04 18:56:26 -0800

    added pokemon persistence

--- app/controllers/users_pokemons_controller.rb
+++ app/controllers/users_pokemons_controller.rb
@@ -1,8 +1,18 @@
 class UsersPokemonsController < ApplicationController
+  def index
+    users_pokemon = UsersPokemon.all
+    render json: pokemons
+  end
+  
   def create
     users_pokemon = UsersPokemon.new(users_pokemon_params)
     users_pokemon.save
     render json: users_pokemon
+  end
+
+  def find_users_pokemons
+    @pokemons = UsersPokemon.find_my_pokemons(params[:user_id])
+    render json: @pokemons
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users_pokemons
   resources :pokemons
   resources :users
+  get '/users/:id/pokemons', to: "users_pokemons#find_users_pokemons"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
